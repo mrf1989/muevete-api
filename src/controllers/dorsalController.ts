fdef38a0dc8969e1add364cd4edab09312d99fb6
mrf1989 <mruano@us.es>
2022-04-06 21:53:51 +0200

    Implementa dorsal
    Incluye la inscripción al evento deportivo, creando un dorsal único para el usuario, así como la posibilidad de editar el lema del dorsal. Falta incluir algunos tests del servicio de creación que trate las excepciones

---
+++ src/controllers/dorsalController.ts
@@ -0,0 +1,22 @@
+import { Controller, POST, PUT, RouteParam, RequestBody } from "../../deps.ts";
+import { DorsalService } from "../services/services.ts";
+import { Dorsal } from "../models/models.ts";
+
+@Controller("/api")
+export class DorsalController {
+    constructor(private readonly dorsalService: DorsalService) {}
+
+    @POST("/dorsal")
+    public async createDorsal(@RequestBody() payload: Dorsal) {
+        try {
+            await this.dorsalService.createDorsal(payload);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    @PUT("/dorsal/:id")
+    public async updateDorsal<T extends Dorsal>(@RouteParam("id") id: string, @RequestBody() payload: T) {
+        return await this.dorsalService.updateDorsal(id, payload);
+    }
+}
---
+++ test/dorsal/service.test.ts
@@ -0,0 +1,49 @@
+import { asserts, Bson, Rhum, Stubbed } from "../../deps.ts";
+import { DBManagement } from "../../src/database/mongodb.ts";
+import { DorsalRepository, EventoRepository } from "../../src/repositories/repositories.ts";
+import { DorsalService, EventoService } from "../../src/services/services.ts";
+import { Dorsal } from "../../src/models/models.ts";
+
+Rhum.testPlan("Testing Dorsal Service", () => {
+    let dbManagement: Stubbed<DBManagement>;
+    const dorsal: Dorsal = {
+        _id: new Bson.ObjectID("6235d572c077516a2dffa8ff"),
+        lema: "Caminar para vencer",
+        num: 34,
+        usuario_id: new Bson.ObjectID("6235d572c077516a2dffa836"),
+        evento_id: new Bson.ObjectID("6235d572c077516a2dffa823")
+    }
+    let dorsalRepository: Stubbed<DorsalRepository>;
+    let eventoRepository: Stubbed<EventoRepository>;
+    let eventoService: Stubbed<EventoService>;
+
+    Rhum.beforeAll(() => {
+        dbManagement = Rhum.stubbed(new DBManagement());
+        dbManagement.stub("connect", () => {});
+        dbManagement.stub("getCollection", () => {});
+    });
+    
+    Rhum.testSuite("Operaciones CRUD en eventos", () => {
+        Rhum.beforeEach(() => {
+            dorsalRepository = Rhum.stubbed(new DorsalRepository(dbManagement));
+            eventoRepository = Rhum.stubbed(new EventoRepository(dbManagement));
+            eventoService = Rhum.stubbed(new EventoService(eventoRepository));
+        });
+
+        Rhum.testCase("Permite crear un dorsal", async () => {   
+            eventoService.stub("getEvento", () => true);
+            dorsalRepository.stub("getAll", () => { return [] });
+            dorsalRepository.stub("createDorsal", () => {});
+    
+            const dorsalService = new DorsalService(dorsalRepository, eventoService);
+            const res = await dorsalService.createDorsal({
+                lema: "A por todas!",
+                usuario_id: "6235d572c077516a2dffa836",
+                evento_id: "6235d572c077516a2dffa823"
+            } as unknown as Dorsal);
+            asserts.assertEquals(res, true);
+        });
+    });
+});
+
+Rhum.run();
