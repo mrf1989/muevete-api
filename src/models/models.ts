1395dc6ab8f10e5d83275f2e3a71bfc91710c778
mrf1989 <mruano@us.es>
2022-04-07 19:09:41 +0200

    Implementa el modelo de esfuerzo

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,5 +1,6 @@
 export type { Articulo } from "./articulo.ts";
 export type { Dorsal } from "./dorsal.ts";
+export type { Esfuerzo } from "./esfuerzo.ts";
 export type { Evento } from "./evento.ts";
 export type { Usuario, UserSession } from "./usuario.ts";
 export { Modalidad } from "./modalidad.ts";
