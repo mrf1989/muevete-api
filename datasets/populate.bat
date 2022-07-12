283242ad12b3659b701a3b8cef645eb384342411
Mario Ruano <mrf1989@hotmail.com>
2022-07-12 17:29:47 +0200

    Corrige  script de importación de datasets

--- datasets/populate.bat
+++ datasets/populate.bat
@@ -4,7 +4,7 @@ mongoimport --db=muevete --collection=usuarios --file=./usuarios.json
 mongoimport --db=muevete --collection=eventos --file=./eventos.json
 mongoimport --db=muevete --collection=articulos --file=./articulos.json
 mongoimport --db=muevete --collection=dorsales --file=./dorsales.json
-mongoimport --db=muevete --collection=usuarios --file=./usuarios.json
+mongoimport --db=muevete --collection=esfuerzos --file=./esfuerzos.json
 
 echo Database populated! Enjoy!
 
